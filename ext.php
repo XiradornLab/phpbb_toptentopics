300d610a9a5512c7ebdd51071c4ffff89eaddd96
Xiradorn <xiradorn@gmail.com>
2015-11-05 12:22:07 +0100

    ext.php for validation purpose and version update @Xiradorn

---
+++ ext.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+* 
+* @package StaffIt - Top Ten Topics 
+* @copyright (c) 2014 brunino
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
+
+namespace staffit\toptentopics;
+
+class ext extends \phpbb\extension\base{
+	// code here
+}
